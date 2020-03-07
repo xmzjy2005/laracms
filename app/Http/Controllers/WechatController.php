01cca27a042a67f3d43110c60ccd0747677a60e4
xmzjy2005 <382338766@qq.com>
2020-03-07 21:51:34 +0800

    wechat

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use Houdunwang\WeChat\WeChat;
 use Illuminate\Http\Request;
+use Modules\Wx\Entities\WxConfig;
 
 class WechatController extends Controller
 {
     //
-    public function handler(){
-    
+    public function handler(WxConfig $wxConfig){
+        $config = array_merge(include base_path('config') . '/wechat.php', $wxConfig->pluck('name', 'value')->toArray());
+        WeChat::config($config)->valid();
     }
 }
